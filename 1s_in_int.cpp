650300273576b4266eaa293b4854d7c01e455591
Shiyao Qi <qishiyao2008@126.com>
2014-04-14 20:33:42 +0800

    Add another implementation code.

--- 1s_in_int.cpp
+++ 1s_in_int.cpp
@@ -1,14 +1,31 @@
-#include<iostream>
-using namespace std;
+/*
+ * Algorithm: Delete the last bit of 1 step by step, until the number is zero.
+ */
+// First implementation.
+#include<stdio.h>
 
 int main() {
     int a, b;
     int count = 0;
-    cin>>a;
+    scanf("%d", &a);
     while(a) {
         count++;
-        b = a & -a;
-        a -= b;
+        b = a & -a; // The last bit of 1.
+        a -= b; // Minus the last bit of 1.
     }	
-    cout<<count<<endl;
+    printf("%d\n", count);
+}
+
+// Second implementation.
+#include<stdio.h>    
+    
+int main() {    
+    int a, b;    
+    int count = 0;    
+    scanf("%d", &a);    
+    while(a) {    
+        count++;    
+        a &= (a -1); // Minux the last bit of 1.
+    }    
+    printf("%d\n", count);    
 }
