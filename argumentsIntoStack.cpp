fa94fee8c6ee2cc52350ab97c39ba87ed74d60c6
Shiyao Qi <qishiyao2008@126.com>
2014-06-28 10:42:32 +0800

    Update argumentsIntoStack.cpp
    Update argumentsIntoStack.cpp file.

--- argumentsIntoStack.cpp
+++ argumentsIntoStack.cpp
@@ -6,6 +6,10 @@ int main()
 	int arr[] = { 6, 7, 8, 9, 10 };
 	int *ptr = arr;
 	*(ptr++) += 123;
-	printf("%d,%d\n", *ptr, *(++ptr)); // the arguments are pushed into a stack, so the last one is the first to execute.
+	
+	// printf() read from left to right, the first read arguments go to bottom of the stack,
+        // the last read arguments go to top of the stack, and then the arguments are popped out.
+        // that is the order of the arguments are dealt from right to left.
+ 	printf("%d,%d\n", *ptr, *(++ptr));
 	return 0;
 }
