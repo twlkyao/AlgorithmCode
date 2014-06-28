6a28f1128faa29f9e59c5d56b3bf29f17efb6801
Shiyao Qi <qishiyao2008@126.com>
2014-06-28 11:44:11 +0800

    Update functionArgumentsIntoStack file.
    Update functionArgumentsIntoStack file.

--- functionArgumentsIntoStack.cpp
+++ functionArgumentsIntoStack.cpp
@@ -11,5 +11,15 @@ int main(int argc, char *argv[]){
 	// the function's arguments into stack's order is right to left, 
 	//the stack bottom's address is bigger than the top.
  	foo(100, 200, 300); 
+ 	int arr[] = { 6, 7, 8, 9, 10 };
+	int *ptr = arr;
+	*(ptr++) += 123;
+
+	// printf() read from right to left, the first read argument goes to top of the stack,
+        // the last read argument goes to bottom of the stack, and then the arguments are popped out.
+        // in the following code, the second argument is calculated before pushed into the stack.
+        // there are five types of push arguments into stack. More info:
+        // http://www.cnblogs.com/xkfz007/archive/2012/03/27/2420158.html
+  	printf("%d,%d\n", *ptr, *(++ptr));
     return 0;
 }
